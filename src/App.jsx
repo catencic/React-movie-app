da5ca999735d4bc3a429783230e3bf24a35f508d
Cristian Atencia <cristianatencia220673@correo.itm.edu.co>
2022-01-29 14:19:40 -0500

    add folder pages, navigatión whit react router and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 
+import { Container } from '@material-ui/core';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { Header } from './components/header/Header';
+import { MainNav } from "./components/MainNav";
+import { Trending } from "./Pages/Trending/Trending";
+import { Movies } from "./Pages/Movies/Movies";
+import { Series } from "./Pages/Series/Series";
+import { Search } from "./Pages/Search/Search";
 
 import './App.css'
 
 
-import React from 'react';
-import { Header } from './components/header/Header';
 
 export const App = () => {
   return (
-    <>
-      <Header />
+    <BrowserRouter > 
+    <Header />
     <div className="app">
-    
+      <Container>
+        <Routes>
+          <Route path="/" element={<Trending/>}  />
+          <Route path="/movies" element={<Movies/>} />
+          <Route path="/series" element={<Series/>} />
+          <Route path="/search" element={<Search/>} />
+        </Routes>
+      </Container>
     </div>
-    </>
+    <MainNav />
+  </BrowserRouter>
   )
 };
---
+++ src/Pages/Series/Series.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Series = () => {
+  return <h1>Series</h1>;
+};
